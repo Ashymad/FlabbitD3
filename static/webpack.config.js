73e3bc0697e35f07a9057c88a42cc06f434b5c9b
Ashymad <czilukim@o2.pl>
2018-05-05 00:37:26 +0200

    Make the chart work with D3 5

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,24 +1,19 @@
-const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module: {
-    rules: [
-	{
-	    test: /\.js$/,
-	    exclude: /(node_modules|bower_components)/,
-	    use: {
-		loader: 'babel-loader',
-		options: {
-		    presets: ['@babel/preset-env']
-		}
-	    }
-	}
-    ]
-}
-
 module.exports = {
-    entry: './src/index.js',
+    module: {
+	rules: [
+	    {
+		test: /\.js$/,
+		exclude: /(node_modules|bower_components)/,
+		use: [ 'babel-loader' ]
+	    },
+	    {
+		test: /\.css$/,
+		use: [ 'style-loader', 'css-loader' ]
+	    }
+	]
+    },
     output: {
 	path: __dirname + '/dist',
 	filename: 'index_bundle.js'
@@ -28,6 +23,5 @@ module.exports = {
 	    title: 'D3 and RabbitMQ',
 	    template: './src/index.html'
 	})
-    ],
-    mode: 'development'
+    ]
 }
